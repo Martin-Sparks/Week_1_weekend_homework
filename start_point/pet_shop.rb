3625e67c971e94a3f485db9f1a5e60c713e5e4e2
Martin-Sparks <martin.sparks82@gmail.com>
2020-04-05 21:38:17 +0100

    initial push

---
+++ start_point/pet_shop.rb
@@ -0,0 +1,91 @@
+def pet_shop_name(pet_shop)
+    return pet_shop[:name]
+end 
+
+def total_cash(pet_shop)
+    return pet_shop[:admin][:total_cash]
+end
+
+def add_or_remove_cash(pet_shop,value)
+ return pet_shop[:admin][:total_cash] += value
+end 
+
+def pets_sold(pet_shop)
+    return pet_shop[:admin][:pets_sold]
+end
+
+def increase_pets_sold(pet_shop, amount)
+    return pet_shop[:admin][:pets_sold] += amount 
+end 
+
+def stock_count(pet_shop)
+    return pet_shop[:pets].count
+end 
+
+def pets_by_breed(pet_shop, breed)
+    pets_found_by_breed = []
+    for pet in pet_shop[:pets] do
+        if pet[:breed] == breed
+            pets_found_by_breed << pet 
+        end 
+    end
+    return pets_found_by_breed
+end 
+
+def find_pet_by_name(pet_shop, name)
+    for pet in pet_shop[:pets] do
+        if pet[:name] == name
+            return pet
+        end
+    end 
+    return nil
+end
+
+# TEST_12
+
+def remove_pet_by_name(pet_shop, name)
+    new_pets=[]
+    count = 0
+    for pet in pet_shop[:pets] do 
+        if pet[:name] == name
+            new_pets == pet_shop[:pets].delete_at(count)
+        else 
+            count += 1
+        end
+    end
+    return new_pets
+end 
+
+def add_pet_to_stock(pet_shop, new_pet)
+     pet_shop[:pets] << new_pet
+     return pet_shop
+end
+
+def customer_cash(customers)
+   return customers[:cash]
+end
+
+def remove_customer_cash(customer, amount)
+    customer[:cash] -= amount
+    return customer[:cash]
+end 
+
+def customer_pet_count(customer)
+    customer[:pets].count
+end
+
+def add_pet_to_customer(customer, new_pet)
+    customer[:pets] << new_pet
+end
+
+def customer_can_afford_pet(customer, new_pet)
+        if customer[:cash] >= new_pet[:price]
+            return true 
+        else 
+            return false 
+    end
+end
+
+def find_pet_by_name(pet_shop, pet_name)
+    
+end 
